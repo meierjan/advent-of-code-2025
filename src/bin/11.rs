b1982a012db460f0382601eb9d45b77deedbc449
Jan Meier <jan@meier.wtf>
2026-01-04 12:45:12 +0100

    Solve Day 11 - Part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 advent_of_code::solution!(11);
 
@@ -41,7 +41,7 @@ pub fn part_one(input: &str) -> Option<u64> {
                     if *next_step == end {
                         exits += 1;
                     } else {
-                        let mut path_with_new_step = next_steps.clone();
+                        let mut path_with_new_step = prev_steps.clone();
                         path_with_new_step.push(next_step);
 
                         to_visit.push_back((path_with_new_step, distance + 1));
@@ -55,8 +55,72 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(exits)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let parsed_input = parse(input);
+    let mut path_map = HashMap::new();
+    for (from, to) in parsed_input {
+        path_map.insert(from, to);
+    }
+
+    let start = "svr";
+    let end = "out";
+
+    let fft = "fft";
+    let dac = "dac";
+
+    let mut cache: HashMap<(&str, &str), u64> = HashMap::new();
+
+    let fft_2_dac = part2_rek(&path_map, fft, dac, &mut cache);
+
+    let result: u64;
+    if fft_2_dac > 0 {
+        result = part2_rek(&path_map, start, fft, &mut cache)
+            * part2_rek(&path_map, fft, dac, &mut cache)
+            * part2_rek(&path_map, dac, end, &mut cache);
+    } else {
+        result = part2_rek(&path_map, start, fft, &mut cache)
+            * part2_rek(&path_map, dac, fft, &mut cache)
+            * part2_rek(&path_map, fft, end, &mut cache);
+    }
+    return Some(result);
+}
+
+pub fn part2_rek<'a>(
+    input: &HashMap<&'a str, Vec<&'a str>>,
+    src: &'a str,
+    out: &'a str,
+    cache: &mut HashMap<(&'a str, &'a str), u64>,
+) -> u64 {
+    if src == out {
+        1u64
+    } else {
+        let children = input.get(src);
+
+        match children {
+            Some(children) => {
+                let sum = children
+                    .into_iter()
+                    .map(|c| {
+                        let result: u64;
+
+                        let c_opt = cache.get(&(*c, out));
+
+                        match c_opt {
+                            Some(c) => result = *c,
+                            None => {
+                                result = part2_rek(input, c, out, cache);
+                                cache.insert((c, out), result);
+                            }
+                        }
+
+                        result
+                    })
+                    .sum();
+                sum
+            }
+            None => 0u64,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -71,7 +135,21 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let example_b = "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out";
+
+        let result = part_two(example_b);
+        assert_eq!(result, Some(2));
     }
 }
